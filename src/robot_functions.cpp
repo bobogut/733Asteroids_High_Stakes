d12e0bf4c825f7b407792e99c83d8b4aeb311234
bobogut <aidenhyao@gmail.com>
2024-12-02 18:42:12 -0800

    Updated color sorter and "bringing the hooks down"

--- src/robot_functions.cpp
+++ src/robot_functions.cpp
@@ -1,5 +1,8 @@
-// Lemlib chassis library used for chassis movement. Relevant PROS library for controller inputs, optical outputs, motor control, 
-// and pneumatic control (embeded in various other includes).
+// The LemLib chassis library used for chassis movement. Relevant PROS library for controller inputs, optical outputs, motor control, and pneumatic control (embeded in various other 
+// includes). We are using the Using standard C++ array library to easily return information and the standard C++ math library to do calculations.
+#include <array>
+#include <cmath>
+
 #include "lemlib/chassis/chassis.hpp"
 
 #include "global_var.h"
@@ -11,6 +14,88 @@
 
 bool overrideColorSort = false;
 bool flipColorSort = false;
+
+// Top hook positions: 1st = 2.0 / 6.0, 2nd = 17.0 / 6.0, 3rd = 33.0 / 6.0
+// Neural hook position: 1st = 0.0, 2nd = 15.0 / 6.0, 3rd = 31.0 / 6.0
+// The hooks should be at the same positions every 46.0 / 6.0 rotations or 7.6666... rotations (Numbers are based on chains over knobs on a sprocket)
+
+// Function to find potential hook positions where they are in a "top position" (i.e. where hooks are at their peak)
+std::array<double, 3> nextTopHook() {
+    double fullRotations = std::floor(intakeHook.get_position() * 6.0 / 46.0); // Used to find how many full rotations have been completed
+
+    if (intakeHook.get_position() * 6.0 / 46.0 - fullRotations >= (33.0 / 46.0)) // Only relevant to bringing a hook to its peak. If the hooks have not completed a full rotation yet the
+        fullRotations += 1;                                                      // the third hook has already passed its peak we have to look at the next rotation otherwise the intake
+                                                                                 // goes backwards. When there is a remainder of 33 / 46 or 0.71739... (percentage of the rotation travelled 
+                                                                                 // after the third hook) we know this is the case.
+
+    double firstHookPosition = (2.0 / 6.0) + (46.0 / 6.0) * fullRotations; // Math for calculating where the hooks could be. The logic is explained above.
+    double secondHookPosition = (17.0 / 6.0) + (46.0 / 6.0) * fullRotations;
+    double thirdHookPosition = (33.0 / 6.0) + (46.0 / 6.0) * fullRotations;
+
+    return {firstHookPosition, secondHookPosition, thirdHookPosition}; // Returns an array of the potential positions for use elsewhere in the code
+}
+
+// Function to find potential hook positions where they are in a "neutral position" (i.e. where hooks are relatively parallel to the ground to avoid the first rung of the ladder). All of
+// the logic is the same as the logic used for finding potential top hook
+std::array<double, 3> nextNeutralHook() {
+    double fullRotations = std::floor(intakeHook.get_position() * 6.0 / 46.0); 
+
+    double firstHookPosition = (46.0 / 6.0) * fullRotations;
+    double secondHookPosition = (15.0 / 6.0) + (46.0 / 6.0) * fullRotations;
+    double thirdHookPosition = (31.0 / 6.0) + (46.0 / 6.0) * fullRotations;
+
+    return {firstHookPosition, secondHookPosition, thirdHookPosition};
+}
+
+// For clarity when I am writing the code
+enum desiredHookPositions {
+    Top,
+    Neutral
+};
+
+double findClosestHook(desiredHookPositions desiredHookPosition) {
+    double desiredPosition;                   // Where we want the intake to move to
+    std::array<double, 3> potentialPositions; // An array for storing all potentially viable hook positions
+
+    if (desiredHookPosition == Top) {
+        potentialPositions = nextTopHook();
+
+        // Runs through every item of the potential positions array
+        for (int i = 0; i < potentialPositions.size(); i++) {
+            if (potentialPositions[i] - intakeHook.get_position() > 0) // If the value is negative we know that the hook associated with the potential position has already passed the 
+                desiredPosition = potentialPositions[i];               // optical sensor and thus cannot hold the ring. As the potential positions array is arranged from smallest to
+                                                                       // largest, we can confidently assume that the first valid position matches to the correct hook.
+                    
+        }
+        
+    }
+    else if (desiredHookPosition == Neutral) {
+        potentialPositions = nextNeutralHook();
+        
+        for (int i = 2; i < potentialPositions.size(); i--) {
+            if (potentialPositions[i] - intakeHook.get_position() <= 0) // If the value is positive we know that the hook associated with the potential position needs to move forward to   
+                desiredPosition = potentialPositions[i];                // reach a "neutral position". As we want the intake to move backwards to avoid catching on mogos, we can 
+                                                                        // immediately discard it. The equal check is to catch when the intake has not moved yet we accidently call it to 
+                                                                        // reach a neutral position to avoid breaking the programming. We run the loop backwards as it is the quickest way 
+                                                                        // to sort through all the potential positions without additional checks. As stated earlier the potential positions                                                                         // 
+                                                                        // array is arragned from smallest to largest, so we can apply the same logic here.
+        }
+    }
+
+    return desiredPosition;
+}
+
+void moveIntakeToDesiredPosition(desiredHookPositions desiredHookPosition) {
+    double desiredPosition = findClosestHook(desiredHookPosition); // Find out where the intake needs to rotate to in order to properly position a hook.
+
+    intakeHook.move_absolute(desiredPosition, 600); // Starts rotating the intake towards the position.
+
+    while (std::abs(desiredPosition - intakeHook.get_position()) > 0.0125) // While error is greater than 0.0125 keep waiting 5 milliseconds to give the move_absolute function time.
+        pros::delay(5);
+
+    if (desiredHookPosition == Top) // If we want to throw a ring off (color sort) then we want a delay to ensure the ring's momentum properly carries it off the hooks
+        pros::delay(500);
+}
 
 void updateIntakeStates() {
     // All the code for controllering the intake state
@@ -33,10 +118,7 @@ void updateIntakeStates() {
         overrideColorSort = !overrideColorSort;
     
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)) // When the up button is pressed flip the color the color sorter looks for
-    {
         storeRing = true;
-        std::cout << "Hello there" << std::endl;
-    }
 
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) // When the B button is pressed find the next valid "down" position
         findNextDown = true;
@@ -62,17 +144,18 @@ bool checkForColor(bool opposite) {
 }
 
 void handleIntake() {
-    double nextTopPosition = 0.32; // First position where hooks are at the top (in rotations)
-    double nextDownPosition = 0; // First position where hooks are right before the top (in rotations)
-
     /*
+    Variables for debugging
+
     double highestHue = 18;
     double lowestHue = 17;
     */
 
     while (true)
     {
-        /* Debug loggers
+        /* 
+        Loggers for debugging
+
         if (optical.get_hue() > highestHue)
             highestHue = optical.get_hue();
         else if (optical.get_hue() < lowestHue && optical.get_hue() != 0)
@@ -83,60 +166,37 @@ void handleIntake() {
         std::cout << "Highest hue is " << highestHue << std::endl;
         */
 
-        
-
-        if (checkForColor(!flipColorSort) && !overrideColorSort) // Check for the opposing alliance ring
-        {
+        // If the driver has not overriden the color sort, look for opposing rings to throw off at the top.
+        if (checkForColor(!flipColorSort) && !overrideColorSort) {
             intakeHook.move_velocity(0); // Stops the intake so that all necessary calcuations can occur
 
-            while (nextTopPosition < intakeHook.get_position()) // Until the next valid position is found
-                nextTopPosition += (15.0 + 1.0 / 3.0) / 6.0;                  // There is a hook at the top every 23/6 (~3.8333) rotations
-
-            intakeHook.move_absolute(nextTopPosition, 600); // Move the hooks to the top of the intake
-
-            while (nextTopPosition - intakeHook.get_position() >= 0.0125) // Delays the code until the intake is within 0.025 revolutions from its desired position so that the move_absolute 
-                pros::delay(5);                            // function has time to run
-
-            std::cout << nextTopPosition - intakeHook.get_position() << std::endl; 
-
-            pros::delay(500); // Creates a half second delay to ensure he ring is flung off
+            moveIntakeToDesiredPosition(Top); // Moves a hook to its top position
         }
 
+        // Same logic as the color sorter except for moving a hook to its neutral position
+        if (findNextDown) { 
+            intakeHook.move_velocity(0);
 
+            moveIntakeToDesiredPosition(Neutral);
 
-        if (findNextDown) // Same logic as the color sorter except for the zero point
-        {
-            intakeHook.move_velocity(0); // Stops the intake so that all necessary calcuations can occur
-
-            while (nextDownPosition < intakeHook.get_position())
-                    nextDownPosition += (15.0 + 1.0 / 3.0) / 6.0;
-
-            nextDownPosition -= (15.0 + 1.0 / 3.0) / 6.0; // Ensures that the hooks will move backwards to avoid getting caught on rings scored on the mogo
-
-            intakeHook.move_absolute(nextDownPosition, 600);
-
-            while (nextDownPosition - intakeHook.get_position() <= -0.0125)
-                pros::delay(5); 
-            
             findNextDown = false;
         }
-        
 
-
+        // This is for storing a ring in the intake when moving towards a goal. Should help with making routes in autonomous and driver controller more convenient 
         if (storeRing) {
-            std::cout << "Hello there" << std::endl;
-
             intakePre.move_velocity(600);
             intakeHook.move_velocity(459);
 
-            while (!checkForColor(false)) {
+            while (!checkForColor(false)) // Wait for a ring to come into the range of the color sensor
                 pros::delay(5);
-            }
 
+            // Stop the intake
             intakePre.move_velocity(0);
             intakeHook.move_velocity(0);
+
             storeRing = false;
         }
+
 
 
         if (intakeState == intakeMogo) // Sets the intake to 600 rpm, running directly to the mogo
@@ -145,14 +205,15 @@ void handleIntake() {
             intakeSpeed = 0;
         else if (intakeState == intakeReverse) // Reverses the intake at 600 rpm
             intakeSpeed = -600;
+        
+        
 
-        if (!overrideIntakeState)
-        {
+        if (!overrideIntakeState) {
             intakePre.move_velocity(intakeSpeed);              // Runs the first stage intake at whatever rpm was set
             intakeHook.move_velocity(intakeSpeed * 459 / 600); // Runs the second stage intake at a slightly slower speed to ensure rings are placed on the mogo properly
         }
 
-        // 459 - 438 = 21 = 11 = 448
+
 
         pros::delay(5); // A delay to ensure all processes run smoothly
     }
@@ -162,12 +223,15 @@ void handleIntake() {
 
 void handlePneumatics() {
     // When the R1 button is pressed toggle the mogo clamp
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1))
-    {
-        if (mogoClamp.is_extended())
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
+
+        if (mogoClamp.is_extended()) // If the clamp is retracted, reverse the intake to a neutral position
             findNextDown = true;
+        
         mogoClamp.toggle();
     }
+
+
 
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) // When the A button is pressed toggle the doinker
         doinker.toggle();
@@ -184,25 +248,26 @@ void handleBase(bool reverse) {
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) // When R2 is pressed toggle the reverse state
             reverse = !reverse;
 
-    if (!reverse) // Regular chassis controls
-    {
+    if (!reverse) {
+        // Regular chassis controls
         leftVelocity = driveCurve.curve(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
         rightVelocity = driveCurve.curve(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
     }
-    else // When the chassis is reversed
-    {
+    else {
+        // When the chassis is reversed
         leftVelocity = driveCurve.curve(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) * -1); // Invert the controllers and swap the sticks
         rightVelocity = driveCurve.curve(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) * -1); // (i.e., left now controls rght and vice versa)
     }
 
 
+    /*
     std::cout << "Left current " << leftMotors.get_current_draw_all()[0] << " " << leftMotors.get_current_draw_all()[1] << " " << leftMotors.get_current_draw_all()[2] << std::endl;
     std::cout << "Right current " << rightMotors.get_current_draw_all()[0] << " " << rightMotors.get_current_draw_all()[1] << " " << rightMotors.get_current_draw_all()[2] << std::endl;
 
 
     std::cout << "Left temp " << leftMotors.get_temperature_all()[0] << " " << leftMotors.get_temperature_all()[1] << " " << leftMotors.get_temperature_all()[2] << std::endl;
     std::cout << "Right temp " << rightMotors.get_temperature_all()[0] << " " << rightMotors.get_temperature_all()[1] << " " << rightMotors.get_temperature_all()[2] << std::endl;
-
+    */
     
     base.tank(leftVelocity, rightVelocity); // Configures the base to have tank control (one joystick direction controls one side of the robot). Simply uses the left and right
                                                         // joysticks.
