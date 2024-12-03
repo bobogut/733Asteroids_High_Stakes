65ace3bdad18de5635804897db75fd61c392c33d
bobogut <aidenhyao@gmail.com>
2024-12-02 19:15:27 -0800

    Cleaned up initialization code and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+// Relevant PROS library are used for tasks and checking motor temperature (embeded in main.h). Other relevant header files are included to declare a task
+// for our intake and declare relevenat motors to modify their attributes.
 #include "main.h"
 
 #include "auton_selector.h"
@@ -8,46 +10,47 @@
 
 void initialize() {
 	autonSelected = none;
-	autonRan = false;
 
 
-
+	// Default intake states
 	intakeState = intakeResting;
 	overrideIntakeState = false;
 	
 
+
+	optical.set_led_pwm(0); // Turn off the LED on the optical sensor to avoid draining too much from the battery
+
+	// Reset the intake motor positions
 	intakePre.tare_position();
 	intakeHook.tare_position();
 
+	// Set the intake motors to coast (i.e. when a movement is over gradually slow down) to avoid burnout, should only add some minor drift
 	intakePre.set_brake_mode(pros::v5::MotorBrake::coast);
 	intakeHook.set_brake_mode(pros::v5::MotorBrake::coast);
 
-	pros::task_t updateIntakeSpeed = pros::Task::create(handleIntake, "Update intake speed"); // Create a task for moving the intake so that
-																							  // the rest of the code will not potentially be
-																							  // interrupted
+	pros::task_t updateIntakeSpeed = pros::Task::create(handleIntake, "Update intake speed"); // Create a task for moving the intake so that the rest of the code will not 
+																											  // potentially be interrupted
 
 
 
-	optical.set_led_pwm(0);
-
-
+	// Same logic as before but for the base motors and tracking wheels
+	leftMotors.tare_position_all();
+	rightMotors.tare_position_all();
 
 	leftMotors.set_brake_mode_all(pros::v5::MotorBrake::coast);
 	rightMotors.set_brake_mode_all(pros::v5::MotorBrake::coast);
 
-	leftMotors.tare_position_all();
-	rightMotors.tare_position_all();
-
 	verticalRotationSensor.reset_position();
 	horizontalRotationSensor.reset_position();
 
-	std::cout << "Vertical is " << verticalRotationSensor.get_position() << std::endl;
-	std::cout << "Horizontal is " << horizontalRotationSensor.get_position() << std::endl;
+	base.calibrate(); // Calibrates the sensors for the base
 
+	// Useful for checking when our base is overheating so that we can let it rest
+	if (leftMotors.is_over_temp_all()[0] || leftMotors.is_over_temp_all()[1] || leftMotors.is_over_temp_all()[2])
+		controller.rumble("- _ _");
 
-	base.calibrate(); // Calibrates the sensors for the chassis
-
-	std::cout << "Initialization done" << std::endl;
+	if (rightMotors.is_over_temp_all()[0] || rightMotors.is_over_temp_all()[1] || rightMotors.is_over_temp_all()[2])
+		controller.rumble("_ - -");
 }
 
 
@@ -57,5 +60,5 @@ void disabled() {}
 
 
 void competition_initialize() {
-	autonSelectorInitialization();
+	autonSelectorInitialization(); // Creates buttons on the brain screen for selecting autonomous routines
 }
