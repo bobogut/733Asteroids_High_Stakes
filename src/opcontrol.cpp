dcaa33b227dd2e5bdc012f1a8e42e213e8071c3c
bobogut <aidenhyao@gmail.com>
2024-12-02 18:49:00 -0800

    Cleaned up  driver control code and added comments

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,60 +1,49 @@
+// Relevant PROS library are used for controller inputs, pneumatic control, and checking motor temperature (embeded in main.h). Other relevant header files are included for functions
+// important to driver control.
 #include "main.h"
 
-#include "lemlib/chassis/chassis.hpp"
-
 #include "ports.h"
-#include "pros/misc.h"
 #include "robot_functions.h"
 #include "states.h"
 
 
 
-void initialAutonPosition() {
-    if (autonSelected == redPositiveCorner) {
-        base.setPose(-47.9, -30.5, 0);
-
-        // base.moveToPose(-48.3, -28.3, 287, 1000);
-    }
-    else if (autonSelected == redNegativeCorner) {
-        base.setPose(-52.9, 16.7, 0);
-
-        base.moveToPose(-58.2, 12.5, 55, 1000, {.forwards = false});
-    }
-    else if (autonSelected == blueNegativeCorner) {
-    }
-    else if (autonSelected == bluePositiveCorner) {
-    }
-    else if (autonSelected == skills) {
-    }
-}
-
 void opcontrol() {
 
     bool reverse = false; // Whether or not the controllers for the chassis should be reversed;
 
-    if (autonSelected == skills) {
+    if (autonSelected == skills)
         reverse = true;
-    }
-
-
-    
-    std::string clampOpen;
 
 
 
     while (true) {
         optical.set_led_pwm(100);
 
-        if (!autonRan && controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)) // Before autonomous period begins and the B button is pressed
-            initialAutonPosition();                                                                // Move the robot to its initial autonomous position
 
 
+        if (leftMotors.is_over_temp_all()[0] || leftMotors.is_over_temp_all()[1] || leftMotors.is_over_temp_all()[2])
+            controller.rumble("- _ _");
+
+        if (rightMotors.is_over_temp_all()[0] || rightMotors.is_over_temp_all()[1] || rightMotors.is_over_temp_all()[2])
+            controller.rumble("_ - -");
+
+
+
+        updateIntakeStates();
         
+
+
         handlePneumatics();
 
+        std::string clampOpen;
 
-        
-        updateIntakeStates();
+        if (mogoClamp.is_extended())
+            clampOpen = "Closed";
+        else
+            clampOpen = "Opened";
+
+        controller.set_text(0, 0, clampOpen); // To inform the driver whether or not the clamp is opened or closed when they cannot see it clearly
 
 
         
@@ -64,18 +53,6 @@ void opcontrol() {
         handleBase(reverse);
 
 
-
-        if (mogoClamp.is_extended())
-            clampOpen = "Closed";
-        else
-            clampOpen = "Opened";
-
-        controller.set_text(0, 0, clampOpen);
-
-
-
-        // std::cout << "Vertical is " << verticalRotationSensor.get_angle() << std::endl;
-	    // std::cout << "Horizontal is " << horizontalRotationSensor.get_angle() << std::endl;
 
         pros::delay(5); // A delay for systems to not get overwhelmed and run properly
     }
