43e05c6568e935563f47d6eec5cb5c72b5b4aeb2
bobogut <aidenhyao@gmail.com>
2024-12-06 16:26:52 -0800

    Fixed bugs with motor resetting improperly

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@
 
 
 void initialize() {
+	optical.set_led_pwm(0); // Turn off the LED on the optical sensor to avoid draining too much from the battery
+
+
+
+	base.calibrate(); // Calibrates the sensors for the base
+
+
+
 	autonSelected = NONE;
 
 
@@ -18,11 +26,11 @@ void initialize() {
 	
 
 
-	optical.set_led_pwm(0); // Turn off the LED on the optical sensor to avoid draining too much from the battery
-
 	// Reset the intake motor positions
 	intakePre.tare_position();
 	intakeHook.tare_position();
+
+	std::cout << intakeHook.get_position() << std::endl;
 
 	// Set the intake motors to coast (i.e. when a movement is over gradually slow down) to avoid burnout, should only add some minor drift
 	intakePre.set_brake_mode(pros::v5::MotorBrake::coast);
@@ -43,14 +51,15 @@ void initialize() {
 	verticalRotationSensor.reset_position();
 	horizontalRotationSensor.reset_position();
 
-	base.calibrate(); // Calibrates the sensors for the base
-
 	// Useful for checking when our base is overheating so that we can let it rest
 	if (leftMotors.is_over_temp_all()[0] || leftMotors.is_over_temp_all()[1] || leftMotors.is_over_temp_all()[2])
 		controller.rumble("- _ _");
 
 	if (rightMotors.is_over_temp_all()[0] || rightMotors.is_over_temp_all()[1] || rightMotors.is_over_temp_all()[2])
 		controller.rumble("_ - -");
+
+	if (intakePre.is_over_temp() || intakeHook.is_over_temp())
+		controller.rumble("_ - _");
 }
 
 
