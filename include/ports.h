b43547a2a2834479f53655dac15bf523dc44a85f
bobogut <aidenhyao@gmail.com>
2025-01-21 17:56:49 -0800

    Removed the second intake motor, need to retune PID constants

--- include/ports.h
+++ include/ports.h
@@ -13,15 +13,20 @@
 extern pros::MotorGroup leftMotors;
 extern pros::MotorGroup rightMotors;
 
+extern pros::Rotation verticalRotationSensor;
+extern pros::Rotation horizontalRotationSensor;
+
 extern lemlib::Chassis base;
 
-extern pros::Motor intakePre;
-extern pros::Motor intakeHook;
+
+
+extern pros::MotorGroup intake;
+
+
 
 extern pros::Optical optical;
 
+
+
 extern pros::adi::Pneumatics mogoClamp;
 extern pros::adi::Pneumatics doinker;
-
-extern pros::Rotation verticalRotationSensor;
-extern pros::Rotation horizontalRotationSensor;
--- src/ports.cpp
+++ src/ports.cpp
@@ -8,14 +8,11 @@
 
 
 // Set up for the drivetrain motors (1, 2, 3, 4, 5, and 6) on the left and right sides respectively
-pros::MotorGroup leftMotors({-19, -18, -17}, pros::v5::MotorGears::blue,
-                            pros::v5::MotorUnits::degrees);
-pros::MotorGroup rightMotors({14, 15, 16}, pros::v5::MotorGears::blue,
-                             pros::v5::MotorUnits::degrees);
+pros::MotorGroup leftMotors({-18, -3, -8}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::MotorGroup rightMotors({4, 6,15}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
 
 // Lemlib set up for the full drivetrain
-lemlib::Drivetrain drivetrain
-{
+lemlib::Drivetrain drivetrain {
     &leftMotors,                   // The motors on the left side of the drivetrain
     &rightMotors,                 // The motors on the right side of the drivetrain
     10.305,                        // Track width, i.e. the distance between wheels on opposite sides of the drivetrain
@@ -24,23 +21,23 @@ lemlib::Drivetrain drivetrain
     2                         // The horizontal drift
 };
 
-pros::IMU imu(10); // Set up for the inertial sensor (imu) on port 7
+pros::IMU imu(17); // Set up for the inertial sensor (imu) on port 17
 
 // Set up for the tracking wheels using rotational sensors on port 8 and 9
 pros::Rotation verticalRotationSensor(-8);
 lemlib::TrackingWheel verticalTracking(&verticalRotationSensor, 2.0, 0.5559155); // The second parameter indicates that
-                                                                                                                                      // we are using the VEX 2" anti-static
-                                                                                                                                      // wheels. The third parameter indicates
-                                                                                                                                      // the offset from the center (in this case
-                                                                                                                                      // the vertical offset is accounted for)
+                                                                                                                 // we are using the VEX 2" anti-static
+                                                                                                                 // wheels. The third parameter indicates
+                                                                                                                 // the offset from the center (in this case
+                                                                                                                 // the vertical offset is accounted for)
 
-pros::Rotation horizontalRotationSensor(4);
+pros::Rotation horizontalRotationSensor(16);
 lemlib::TrackingWheel horizontalTracking(&horizontalRotationSensor,
                                          2.0, 2.3059155);
 
-lemlib::OdomSensors sensors // Putting together everything to fully set up odometry in Lemlib
-(
-    &verticalTracking,   // For tracking movement on the y-axis
+// Putting together everything to fully set up odometry in Lemlib
+lemlib::OdomSensors sensors (
+    nullptr,   // &verticalTracking,   // For tracking movement on the y-axis
     nullptr,   // No second vertical tracking wheel
     &horizontalTracking, // For tracking movement on the x-axis
     nullptr, // No second horizontal tracking wheel
@@ -48,12 +45,12 @@ lemlib::OdomSensors sensors // Putting together everything to fully set up odome
 
 );
 
-lemlib::ControllerSettings lateralController // PID constants for moving foward/backward
-(
-    7.5, // 7.5                  // Proportional gains, i.e. the weight of error on the calculation
-    0.1,                    // Integral gains, i.e. the weight of error accumulation on the calculation
-    25, // 25                    // Derivative gains, i.e. the weight of error change on the calculation
-    1.00335, // Anti windup
+// PID constants for moving foward/backward
+lemlib::ControllerSettings lateralController (
+    10, // 7.5                  // Proportional gains, i.e. the weight of error on the calculation
+    0,                    // Integral gains, i.e. the weight of error accumulation on the calculation 0.1
+    0, // 25                    // Derivative gains, i.e. the weight of error change on the calculation
+    0, // Anti windup 1.00335
     0,            // Small error timeout is considered when the robot is within 1" of its goal
     0, // When error is in the small range for long enough move on to the next motion
     0,            // Large error timeout is considered when the robot is within 3" of its goal
@@ -61,27 +58,32 @@ lemlib::ControllerSettings lateralController // PID constants for moving foward/
     0                   // Maximum acceleration
 );
 
-lemlib::ControllerSettings angularController // PID constants for turning
-(
+// PID constants for turning
+lemlib::ControllerSettings angularController (
     4,
-    0.45,
-    30, // 18.75
-    -1.15725333333333,
-    1,100,3,500,0
+    0, // 0.45
+    30, // 30
+    0, //-1.15725333333333
+    0,0,0,0,0 //1,100,3,500
 );
 
-lemlib::Chassis base // Lemlib set up for the full base
-(
+// Lemlib set up for the full base
+lemlib::Chassis base (
     drivetrain,
     lateralController,
     angularController,
     sensors
 );
 
-pros::Motor intakePre(12, pros::v5::MotorGears::blue, pros::v5::MotorEncoderUnits::rotations); // Intake motor
-pros::Motor intakeHook(11, pros::v5::MotorGears::blue, pros::v5::MotorEncoderUnits::rotations);
 
-pros::Optical optical(6); // Optical sensor for ring sorter
+
+pros::MotorGroup intake({-10, 5}, pros::v5::MotorGears::blue, pros::v5::MotorEncoderUnits::rotations);
+
+
+
+pros::Optical optical(1); // Optical sensor for ring sorter
+
+
 
 pros::adi::Pneumatics mogoClamp('a', false); // Technically starts extended due to wiring
 pros::adi::Pneumatics doinker('b', false);
